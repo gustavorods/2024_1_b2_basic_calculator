af293f4ed2ac95e971a2ff32352986f9153cd806
gustavorods <gustavo.rlsilva07@gmail.com>
2024-05-08 15:27:47 -0300

    feat: Objects to be used in the interface

---
+++ src/calculator.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class calculator extends JFrame {
+    JTextField campoNum1, campoNum2;
+    JButton somar, subtrair, multiplicar, dividir, habilitar, desabilitar, ocultar, exibir, sair, limpar;
+    JLabel rotuloNum1, rotuloNum2, resultado, credito;
+
+    public calculator() {
+        super("Calculadora básica");
+    }
+}
